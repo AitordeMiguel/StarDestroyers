c466d30234027ea3f2f8844f6c353bc217f82997
AitordeMiguel <ademiguel022@ikasle.ehu.eus>
2026-04-05 11:32:37 +0200

    Añadido generarEnem a Factory

--- StarDestroyers/src/model/Factory.java
+++ StarDestroyers/src/model/Factory.java
@@ -1,4 +1,5 @@
 package model;
+import java.util.*;
 
 public class Factory {
 	private static Factory miFactory;
@@ -9,16 +10,24 @@ public class Factory {
     	if (miFactory == null){miFactory = new Factory();}
     	return miFactory;
     }
-    public PiezaAbs generar(int tipo, String col, int x, int y)
+    public PiezaAbs generar(int tipo, String col, ArrayList<int[]> LPos)
     {
     	PiezaAbs elem = null;
-    	if(tipo==0) {elem = generarNave(col,x,y);}
-    	else {elem = generarEnem(x,y);}
+    	if(LPos != null)
+    	{
+    		if(tipo==0) 
+        	{
+        		int x = LPos.get(0)[0];
+        		int y = LPos.get(0)[1];
+        		elem = generarNave(col,x,y);
+        	}
+        	else {elem = generarEnem(LPos);}
+    	}
     	return elem;
     }
     
     private Nave generarNave(String color, int x, int y)
-    {
+    {//TODO si queremos que se puedan regenerar naves, hay que comprobar que se pueda crear en el sitio sin que haya otra cosa
     	Nave nave = null;
     	
     	if(x>0 && x<=59 && y>0 && y<99) //Si la nave cabe en el plano
@@ -42,9 +51,29 @@ public class Factory {
     	return nave;
     }
     
-    private Enemigo generarEnem(int x, int y)
-    {
+    private Enemigo generarEnem(ArrayList<int[]> LEnem)
+    {//TODO si queremos que se puedan regenerar naves, hay que comprobar que se pueda crear en el sitio sin que haya otra cosa
     	Enemigo enem = null;
+    	
+    	Composite formaComp = new Composite();
+    	
+    	for(int[] coor: LEnem)
+    	{
+    		int x = coor[0];
+    		int y = coor[1];
+    		
+    		PixelN n1 = new PixelN(x,y);//Centro
+    		PixelN n2 = new PixelN(x+1,y);//Derecha
+    		PixelN n3 = new PixelN(x-1,y);//Izquierda
+    		PixelN n4 = new PixelN(x,y-1);//Arriba
+    		
+    		formaComp.addComponente(n1);
+    		formaComp.addComponente(n2);
+    		formaComp.addComponente(n3);
+    		formaComp.addComponente(n4);
+    		enem = new Enemigo();
+    	}
+    	
     	return enem;
     }
 }
